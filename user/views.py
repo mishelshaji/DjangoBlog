a4c7ccfb15edd8063f0888e5f2bc301b0551c5a8
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-25 09:41:05 +0530

    Rendering HTML pages

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'user/home.html')
 
 def about(request):
-    return HttpResponse("About Us")
+    return render(request, 'user/about.html')

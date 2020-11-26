511e5ed257239f55179d8b7b3e18579e55406a23
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-26 10:12:58 +0530

    Created registration page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('login', user_login, name='login'),
+    path('register', register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
 def user_login(request):
     return render(request, 'accounts/login.html', {"form": AuthenticationForm()})
+
+def register(request):
+    return render(request, 'accounts/register.html', {"form": UserCreationForm()})

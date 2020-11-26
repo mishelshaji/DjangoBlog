f99daecc418490dd522cc82dcc10040c13552d6d
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-26 10:05:13 +0530

    Create login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def user_login(request):
+    return render(request, 'accounts/login.html', {"form": AuthenticationForm()})

94efcd763d71c798f7c031d79d0bda916abf893e
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-27 10:11:43 +0530

    Created books model
    Create the book model and run these commands.
    Run
    - python manage.py makemigrations
    - python manage.py migrate

--- book/models.py
+++ book/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    CATEGORY = (
+        ('science', 'Science'),
+        ('tech', 'Technology'),
+        ('sports', 'Sports'),
+    )
+
+    id = models.BigAutoField(
+        primary_key=True,
+    )
+
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Book Name'
+    )
+
+    author = models.CharField(
+        max_length=150,
+        blank=False,
+        verbose_name='Author'
+    )
+
+    published_on = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name='Published On'
+    )
+
+    price = models.IntegerField(
+        default=0,
+        verbose_name='Price'
+    )
+
+    category = models.CharField(
+        max_length=100,
+        choices=CATEGORY
+    )
+
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_on = models.DateTimeField(
+        auto_now=True
+    )

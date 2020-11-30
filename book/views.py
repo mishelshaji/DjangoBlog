abc533dea5f1a8651fccfc4a31c146ba10ab1bd9
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-30 09:54:06 +0530

    Display Books

--- book/views.py
+++ book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def home(request):
+    books = Book.objects.all()
+    return render(request, 'book/home.html', {'data':books})
+

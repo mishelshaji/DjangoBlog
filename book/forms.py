663e62fa0473b758778cdeada5c541b5f890b8a9
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-30 10:10:32 +0530

    Styling Book creation form

--- book/forms.py
+++ book/forms.py
@@ -1,8 +1,36 @@
-from django.forms import ModelForm
-from django.forms import fields
+from django.forms import ModelForm, TextInput, DateInput, NumberInput, Select
 from .models import Book
 
 class BookCreationForm(ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+        widgets = {
+            'name': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Book Name'
+                }
+            ),
+            'author': TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'published_on': DateInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'date'
+                }
+            ),
+            'price': NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'category': Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }

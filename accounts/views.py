24bfd88ec5e11050fbb8a24a22dc570ae0d74bd1
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-12-07 13:11:36 +0530

    Created login

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 # Create your views here.
 def user_login(request):
-    return render(request, 'accounts/login.html', {"form": AuthenticationForm()})
+    if request.method == "GET":
+        return render(request, 'accounts/login.html', {"form": AuthenticationForm()})
+    else:
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                target = request.GET.get('next')
+                if target is not None:
+                    return redirect(target)
+                return redirect('admin_post_list')
+
 
 def register(request):
     return render(request, 'accounts/register.html', {"form": UserCreationForm()})

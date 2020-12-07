12d171340db9e8e8280d168215ceccc3819a8121
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-12-07 09:52:55 +0530

    Handling post creation

--- administrator/views.py
+++ administrator/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     pass
 
+@login_required
 def post_create(request):
     if request.method == "GET":
         context = {}
         context['form'] = PostForm()
+        return render(request, 'administrator/post_create.html', context)
+    
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Data saved")
+
+        context = {}
+        context['form'] = form
         return render(request, 'administrator/post_create.html', context)

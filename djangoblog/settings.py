28949a5683b20e8baed4867aa897a9ff66fa2be2
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-12-21 10:00:14 +0530

    Serving static files in production

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0vr%2ey#0$_pi-@4kflji55rzjt)b$3$%%38%+ply)%xpv0yx('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -160,3 +160,5 @@ if DEBUG:
             },
         }
     }
+
+STATIC_ROOT = 'staticfiles'

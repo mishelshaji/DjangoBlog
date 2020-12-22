4bb6e9b6355acc05f3f145b2e64eb608fb5a2a2c
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-12-22 10:49:43 +0530

    Removing unused code

--- administrator/views.py
+++ administrator/views.py
@@ -64,14 +64,13 @@ def create_category(request):
         if cf.is_valid():
             name = cf.cleaned_data['name']
             description = cf.cleaned_data['description']
-            obj, has_created = Category.objects.get_or_create(
+            Category.objects.get_or_create(
                 name=name,
                 defaults={
                     'description': description
                 }
             )
-            print(obj)
-            print(has_created)
+
             return redirect('admin_category_list')
         return render(request, 'administrator/category_create.html', {'form': cf})
         
